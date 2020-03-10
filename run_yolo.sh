4d4c466c718f2f985bc861e334cdfcebf0543a1d
István Sárándi <sarandi@vision.rwth-aachen.de>
2020-03-10 18:53:31 +0100

    Add command line args

--- run_yolo.sh
+++ run_yolo.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-shopt -s extglob
+set -euo pipefail
 
-IMG_ROOT=$1
-DESTINATION=$2
-NJOBS=$3
-DARKNET_DIR=.
+while [[ "$#" -gt 0 ]]; do case $1 in
+  --image-root=*) IMG_ROOT="${1#*=}"; shift;;
+  --out-path=*) DESTINATION="${1#*=}"; shift;;
+  --jobs=*) NJOBS="${1#*=}"; shift;;
+  --image-root|--out-path|--jobs) echo "$1 requires an argument" >&2; exit 1;;
+  *) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
 
 if [[ -d $DESTINATION ]]; then
     echo "$DESTINATION must not be a directory, it should be a target file name"
     exit 1
 fi
 
-find "$IMG_ROOT" -name '*.jpg' \
- | sort \
- | parallel --jobs $NJOBS -N1 --round-robin --roundrobin --pipe \
-    "$DARKNET_DIR/darknet" detect "$DARKNET_DIR/cfg/yolov3-spp.cfg" "$DARKNET_DIR/yolov3-spp.weights" -thresh 0.1 \
-    '>>' "${DESTINATION}{%}.txt"
+find "$IMG_ROOT" -name '*.jpg' | sort \
+ | parallel --verbose --jobs "$NJOBS" -N1 --round-robin --pipe \
+    ./darknet detect ./cfg/yolov3-spp.cfg ./yolov3-spp.weights -thresh 0.1 '>>' "${DESTINATION}.raw_{%}"
 
-cat "${DESTINATION}*.txt" > "${DESTINATION}_all.txt"
-rm "${DESTINATION}!(_all).txt"
-$DARKNET_DIR/boxes_to_pickle.py --in-path "${DESTINATION}_all.txt"
+cat "${DESTINATION}.raw_*" > "${DESTINATION}.raw"
+rm "${DESTINATION}.raw_*"
+"$DARKNET_DIR/boxes_to_pickle.py" --in-path "$DESTINATION.raw" --out-path="$DESTINATION" --root-dir="$IMG_ROOT"
